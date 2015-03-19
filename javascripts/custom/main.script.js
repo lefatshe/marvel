3ddd3698001ad3f53627c1ce984a11f614d2b920
lefatshe70 <lefatshe70@yahoo.com>
2015-03-19 14:15:49 +0200

    init - commit - indexing cases

--- javascripts/custom/main.script.js
+++ javascripts/custom/main.script.js
@@ -1,7 +1,54 @@
+
+// Enable toggle panels
 var togglePanels = function(){
 	$(".toggle-title").click(function () {
 		$(this).next(".toggle-details").toggle("fast");
 	});
+}
+// Stick menu item to top
+var stickMenu = function() {
+	var menu = $('.menu');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('.menu').addClass('sticky');
+            $('#Content').addClass('menu-padding');
+        } else {
+            $('.menu').removeClass('sticky');
+            $('#Content').removeClass('menu-padding');
+        }
+
+
+    }
+
+    document.onscroll = scroll;
+}
+// Ensure HTML placeholder tag compatible with IE
+var placeHolder = function() {
+	(function ($) {
+         $.support.placeholder = ('placeholder' in document.createElement('input'));
+     })(jQuery);
+
+
+     //fix for IE7 and IE8
+     $(function () {
+         if (!$.support.placeholder) {
+             $("[placeholder]").focus(function () {
+                 if ($(this).val() == $(this).attr("placeholder")) $(this).val("");
+             }).blur(function () {
+                 if ($(this).val() == "") $(this).val($(this).attr("placeholder"));
+             }).blur();
+
+             $("[placeholder]").parents("form").submit(function () {
+                 $(this).find('[placeholder]').each(function() {
+                     if ($(this).val() == $(this).attr("placeholder")) {
+                         $(this).val("");
+                     }
+                 });
+             });
+         }
+     });
 }
 
 // ***************************************************************
@@ -10,4 +57,6 @@ var togglePanels = function(){
 
 jQuery( document ).ready(function() {
     togglePanels();
+    stickMenu();
+    placeHolder();
 });
